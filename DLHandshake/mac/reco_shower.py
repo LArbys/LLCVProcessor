34a3241d9d12760b7dd2301f4c74b964deea7faf
Victor Genty <vgenty@nevis.columbia.edu>
2017-10-05 17:01:30 -0400

    insert into basepath for tufts

--- DLHandshake/mac/reco_shower.py
+++ DLHandshake/mac/reco_shower.py
@@ -1,12 +1,25 @@
 import os,sys
 
+if len(sys.argv != 3):
+    print 
+    print "PGRAPH_FILE = str(sys.argv[1])"
+    print "HIT_FILE    = str(sys.argv[2])"
+    print 
+    sys.exit(1)
+
+
+PGRAPH_FILE = str(sys.argv[1])
+HIT_FILE    = str(sys.argv[2])
+
+num = int(PGRAPH_FILE.split(".")[0].split("_")[-1])
+
 import ROOT
 from larlitecv import larlitecv
-
 from ROOT import llcv
 
 BASE_PATH = os.path.realpath(__file__)
 BASE_PATH = os.path.dirname(BASE_PATH)
+sys.path.insert(0,BASE_PATH)
 
 #
 # Configure Processor
@@ -22,13 +35,16 @@ proc.add_ll_ana(dlshr3d)
 
 proc.configure(os.path.join(BASE_PATH,"config.cfg"))
 
-proc.add_lcv_input_file(os.path.join(BASE_PATH,"..","tmp","vertexout_larcv_382133380.root"))
-proc.add_ll_input_file(os.path.join(BASE_PATH,"..","tmp","larlite_reco2d_0176.root"))
+proc.add_lcv_input_file(PGRAPH_FILE)
+proc.add_ll_input_file(HIT_FILE)
 
-proc.set_output_ll_name("ll_trash.root")
+proc.set_output_ll_name("shower_reco_out_%d.root" % num)
 
 proc.initialize()
 
+#
+# run over larcv entries
+#
 proc.batch_process_lcv()
 
 proc.finalize()
