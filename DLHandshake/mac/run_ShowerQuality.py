577496cc372e04fb822ac049335c444a3067a37f
Victor Genty <vgenty@nevis.columbia.edu>
2017-11-08 14:46:15 -0500

    update shower dump

--- DLHandshake/mac/run_ShowerQuality.py
+++ DLHandshake/mac/run_ShowerQuality.py
@@ -1,4 +1,12 @@
 import os, sys
+
+if len(sys.argv) != 3:
+    print 
+    print "INFILE  = str(sys.argv[1])"
+    print "OUTFILE = str(sys.argv[2])"
+    print 
+    sys.exit(1)
+
 from larlite import larlite as fmwk
 
 INFILE = str(sys.argv[1])
@@ -7,19 +15,16 @@ num = int(INFILE.split(".")[0].split("_")[-1])
 OUTFILE = os.path.join(OUTDIR,"showerqualsingle_%d.root" % num)
 
 my_proc = fmwk.ana_processor()
-my_proc.add_input_file(sys.argv[1])
+my_proc.add_input_file(INFILE)
 my_proc.set_io_mode(fmwk.storage_manager.kREAD)
 my_proc.set_ana_output_file(OUTFILE)
 my_proc.set_output_file("")
 
-sq_module = fmwk.ShowerQuality_nueshowers()
+sq_module = fmwk.ShowerQuality_DL()
+sq_module.SetVertexProducer("dl")
 sq_module.SetShowerProducer("showerreco")
-sq_module.SetMaxEnergyCut(9e9)
-sq_module.SetMinEnergyCut(0.)
-sq_module.SetSingleParticleQuality(True)
 my_proc.add_process(sq_module)
 
 my_proc.run()
 
 sys.exit(0)
-
