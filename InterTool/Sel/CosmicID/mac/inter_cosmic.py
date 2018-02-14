187bc8fc0eb939e792521b6bcdb015bda2a14da1
Victor Genty <vgenty@nevis.columbia.edu>
2018-02-14 17:21:54 -0500

    update for pubs

--- InterTool/Sel/CosmicID/mac/inter_cosmic.py
+++ InterTool/Sel/CosmicID/mac/inter_cosmic.py
@@ -49,7 +49,7 @@ imod = llcv.InterModule()
 driver = imod.Driver()
 FOUT = "cosmic_ana_%d.root"
 
-num = int(SSNET_FILE.split(".")[0].split("_")[-1])
+num = int(VTX_FILE.split(".")[0].split("_")[-1])
 FOUT = FOUT % num
 
 driver.SetOutputFilename(FOUT)
