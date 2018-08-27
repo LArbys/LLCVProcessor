184a1ca19d0eaefc6d9173aeef5efbe3bb3559a3
Victor Genty <vgenty@nevis.columbia.edu>
2018-08-27 13:30:06 -0400

    add lcv writeout

--- InterTool/Sel/lib/writeout_modules.py
+++ InterTool/Sel/lib/writeout_modules.py
@@ -1,12 +1,20 @@
 import ROOT
 from larlite import larlite as ll
+from larcv import larcv as lcv
 
-def InterWriteOut():
-    name = "InterWriteOut"
+def LL_InterWriteOut():
+    name = "LL_InterWriteOut"
     return ll.InterWriteOut()
 
+def LC_InterWriteOut():
+    name = "LC_InterWriteOut"
+    return lcv.InterWriteOut(name)
+
 def attach_writeout(proc):
-    writeout = InterWriteOut()
-    proc.add_ll_ana(writeout)
+    ll_writeout = LL_InterWriteOut()
+    proc.add_ll_ana(ll_writeout)
+
+    lc_writeout = LC_InterWriteOut()
+    proc.add_lc_proc(lc_writeout)
 
     return
