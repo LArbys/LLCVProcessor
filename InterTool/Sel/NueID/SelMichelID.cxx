64656d3c2758bca8b72362f44511a8ee5c057cdb
Victor Genty <vgenty@nevis.columbia.edu>
2018-08-30 15:01:11 -0400

    testing...

--- InterTool/Sel/NueID/SelMichelID.cxx
+++ InterTool/Sel/NueID/SelMichelID.cxx
@@ -876,7 +876,17 @@ namespace llcv {
 	  LLCV_DEBUG() << "new_edge=(" << new_edge.x << "," << new_edge.y << ")" << std::endl;
 	  ix+=1;
 	}
-	
+
+	// make sure the ege is _not_ outside the image
+	if (new_edge.x >= (float)_cropx) {
+	  LLCV_WARNING() << "new_edge.x=" << new_edge.x << " outside cropx" << std::endl;
+	  new_edge.x = _cropx - 1;
+	}
+	if (new_edge.y >= (float)_cropy)  {
+	  LLCV_WARNING() << "new_edge.y=" << new_edge.y << " outside cropy" << std::endl;
+	  new_edge.y = _cropy - 1;
+	}
+
 	Triangle new_triangle(start,inv_pt,inv_pt);
 	float nline_pixels = 0;
 	float npar_pixels  = 0;
